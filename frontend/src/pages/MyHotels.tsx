9ef7137817d51dd19a8990b02ebf76c47fc9bf12
LulzimTafaj <luitafaj23@gmail.com>
2024-05-10 15:28:47 -0400

    fixed bug

--- frontend/src/pages/MyHotels.tsx
+++ frontend/src/pages/MyHotels.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import * as apiClient from "../api-client";
+import { BsBuilding, BsMap } from "react-icons/bs";
+import { BiHotel, BiMoney, BiStar } from "react-icons/bi";
 
 const MyHotels = () => {
     const { data: hotelData } = useQuery(
@@ -18,10 +20,10 @@ const MyHotels = () => {
     return (
         <div className="space-y-5">
             <span className="flex justify-between">
-                <h1 className="text-3xl font-bold text-slate-700">My Hotels</h1>
+                <h1 className="text-4xl font-bold text-slate-700 font-sans ml-8">My Hotels</h1>
                 <Link
                     to="/add-hotel"
-                    className="flex bg-rose-700 text-white text-md font-semibold rounded-sm p-2 hover:bg-rose-600"
+                    className="flex bg-rose-700 text-white text-lg font-semibold rounded-sm p-2 hover:bg-rose-600 mr-6"
                 >
                     Add Hotel
                 </Link>
@@ -34,24 +36,26 @@ const MyHotels = () => {
                     >
                         <h2 className="text-2xl font-semibold text-white">{hotel.name}</h2>
                         <div className="whitespace-pre-line text-white">{hotel.description}</div>
-                        <div className="grid grid-cols-5 gap-2 text-white">
-                            <div className="border border-slate-300 rounded-md p-3 flex items-center">
-                                {hotel.city}, {hotel.country}
+                        <div className="grid grid-cols-5 gap-2 text-white text-sm">
+                            <div className="border border-slate-300 rounded-md p-3 flex items-center justify-center overflow-hidden">
+                                <div className="whitespace-nowrap flex items-center"><BsMap className="mr-2" />{hotel.city}, {hotel.country}</div>
                             </div>
-                            <div className="border border-slate-300 rounded-md p-3 flex items-center">
-                                {hotel.type}
+                            <div className="border border-slate-300 rounded-md p-3 flex items-center justify-center overflow-hidden">
+                                <BsBuilding className="mr-1" />
+                                <div className="nowrap">{hotel.type}</div>
                             </div>
-                            <div className="border border-slate-300 rounded-md p-3 flex items-center">
-                                {hotel.facilities}
+                            <div className="border border-slate-300 rounded-sm p-3 flex items-center justify-center overflow-hidden">
+                                <BiMoney className="mr-1" />${hotel.pricePerNight} per night
                             </div>
-                            <div className="border border-slate-300 rounded-md p-3 flex items-center">
-                                {hotel.adultCount} adults, {hotel.childCount} children
+                            <div className="border border-slate-300 rounded-md p-3 flex items-center justify-center overflow-hidden">
+                                <div className="whitespace-nowrap flex items-center">  <BiHotel className="mr-1" />{hotel.adultCount} adults, {hotel.childCount} children</div>
                             </div>
-                            <div className="border border-slate-300 rounded-md p-3 flex items-center">
-
+                            <div className="border border-slate-300 rounded-md p-3 flex items-center justify-center overflow-hidden">
+                                <BiStar className="mr-1" />
                                 {hotel.starRating} Star Rating
                             </div>
                         </div>
+
                         <span className="flex justify-end">
                             <Link
                                 to={`/edit-hotel/${hotel._id}`}
